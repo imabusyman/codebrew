367ddaa24ade688948c2a8ff7528a6a0a1341dcf
Allen Peterson <Allen.Peterson@toro.com>
2024-02-15 10:11:33 -0600

    ug

--- src/CodeBrew.AppHost/Program.cs
+++ src/CodeBrew.AppHost/Program.cs
@@ -1,6 +1,6 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
 
-builder.AddProject<CodeBre>("codebrew.letstravelit.api");
+builder.AddProject<Projects.CodeBrew_LetsTravelIt_Api>("codebrew.letstravelit.api");
 
 builder.Build().Run();
--- src/CodeBrew.LetsTravelIt.Api/Program.cs
+++ src/CodeBrew.LetsTravelIt.Api/Program.cs
@@ -1,7 +1,29 @@
-﻿using CodeBrew.LetsTravelIt.Shared.Models;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddLetsTravelItApi();
+builder.AddServiceDefaults();
 
-WebApplication app = builder.Build();
-app.UseLetsTravelItApi();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
